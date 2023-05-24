aed6afb2cd53d86df58c35cbe7a25311b4ba822a
daevata <daevata@mail.ru>
2023-05-24 15:29:15 +0900

    Added post method

--- barbershop.rb
+++ barbershop.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+  @user_name = params[:user_name]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = "Hello"
+  @message = "Dear #{@user_name}, we`ll be waiting you at #{@date_time}"
+  erb :message
+end

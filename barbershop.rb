932d2f0d872581dc398b76a00b4f3e275732054a
daevata <daevata@mail.ru>
2023-05-26 12:34:00 +0900

    Added styles.css with font-family inside

--- barbershop.rb
+++ barbershop.rb
@@ -4,6 +4,10 @@ get '/' do
   erb :index
 end
 
+get '/admin' do
+  erb :admin
+end
+
 post '/' do
   @user_name = params[:user_name]
   @phone = params[:phone]
@@ -12,7 +16,23 @@ post '/' do
   @title = "Hello"
   @message = "Dear #{@user_name}, we`ll be waiting you at #{@date_time}"
 
-  f = File.open 'users.txt', 'a'
+  f = File.open ./public/'appointment.txt', 'a'
   f.write "Client: #{@user_name}, Phone: #{@phone}, Appointment Date: #{@date_time}\n"
   erb :message
+end
+
+post '/' do
+  @login = params[:login]
+  @password = params[:password]
+  @title = "Hello Mr.Admin"
+
+  if @login == "Admin" and @password == "Secret"
+    erb :admin
+  elsif
+    @login == "Admin" and @password == "Admin"
+    @message = "Nice try"
+    erb :index
+  else
+    erb :index
+  end
 end
